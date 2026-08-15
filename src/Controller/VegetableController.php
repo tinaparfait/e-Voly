c6aecc8b4ed88a60388b3f3a23569582a45a681a
tinaparfait <rasolofo.tinaparfait@gmail.com>
2026-08-15 11:33:40 +0200

    Add new features list and details vegetable

--- src/Controller/VegetableController.php
+++ src/Controller/VegetableController.php
@@ -15,23 +15,37 @@ use Symfony\Component\Routing\Attribute\Route;
 final class VegetableController extends AbstractController
 {
     private EntityManagerInterface $em;
+    private ManagerRegistry $doctrine;
 
-    public function __construct(EntityManagerInterface $em)
-    {
+    public function __construct(
+        EntityManagerInterface $em,
+        ManagerRegistry $doctrine,
+    ) {
         $this->em = $em;
+        $this->doctrine = $doctrine;
     }
 
     // Affiche tout listes de légumes
-    #[Route('/', name: 'app_vegetable_client')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/', name: 'app_vegetable_list')]
+    public function list(): Response
     {
-        $repository = $doctrine->getRepository(Vegetable::class);
+        $repository = $this->doctrine->getRepository(Vegetable::class);
         $vegetable = $repository->findAll();
 
-        return $this->render('vegetable/vegetable.html.twig', [
+        return $this->render('vegetable/list.html.twig', [
             'vegetables' => $vegetable,
         ]);
     }
+
+    // Afficher un légume particulier
+    #[Route('/{id}', name: 'app_vegetable_details')]
+    public function details(Vegetable $vegetable): Response
+    {
+        return $this->render('vegetable/details.html.twig', [
+            'vegetable' => $vegetable,
+        ]);
+    }
+
 
     // Une méthode qui permet d'ajouté un légume
     #[Route('/ajouter', name: 'app_add_vegetable')]
